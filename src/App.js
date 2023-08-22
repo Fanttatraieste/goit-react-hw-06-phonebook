eeeaf2af06755a644201bbb2a603bf08579451be
Fanttatraieste <118627923+Fanttatraieste@users.noreply.github.com>
2023-08-22 16:56:08 +0300

    cv inainte de tema

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import CreateCustomer from './features/customer/CreateCustomer';
+import Customer from './features/customer/Customer';
+import AccountOperations from './features/account/AccountOperations';
+import BalanceDisplay from './features/account/BalanceDisplay';
+import { useSelector } from 'react-redux';
+
+function App() {
+  const name = useSelector(state => state.customer.fullName);
+
+  return (
+    <div>
+      <h1>🏦 The React-Redux Bank ⚛️</h1>
+      {/* <CreateCustomer />
+      <Customer />
+      <AccountOperations />
+      <BalanceDisplay /> */}
+      {!name && <CreateCustomer />}
+      {name && (
+        <>
+          <Customer />
+          <AccountOperations />
+          <BalanceDisplay />
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
