328a60866c10cb1de8ffe42992fbd77c7b962a8c
Fanttatraieste <118627923+Fanttatraieste@users.noreply.github.com>
2023-08-22 17:36:42 +0300

    inca cv

--- src/features/account/AccountOperations.js
+++ src/features/account/AccountOperations.js
@@ -17,8 +17,9 @@ function AccountOperations() {
   function handleDeposit() {
     if (!depositAmount) return;
 
-    dispatch(deposit(depositAmount));
+    dispatch(deposit(depositAmount, currency));
     setDepositAmount('');
+    setCurrency('USD');
   }
 
   function handleWithdrawal() {
@@ -58,7 +59,9 @@ function AccountOperations() {
             <option value="GBP">British Pound</option>
           </select>
 
-          <button onClick={handleDeposit}>Deposit {depositAmount}</button>
+          <button onClick={handleDeposit} disabled={account.isLoading}>
+            Deposit {depositAmount}
+          </button>
         </div>
 
         <div>
--- src/features/account/accountSlice.js
+++ src/features/account/accountSlice.js
@@ -2,12 +2,19 @@ const initialStateAccount = {
   balance: 0,
   loan: 0,
   loanPurpose: '',
+  isLoading: false,
 };
 
 export default function accountReducer(state = initialStateAccount, action) {
   switch (action.type) {
+    case 'account/convertingCurrency':
+      return { ...state, isLoading: true };
     case 'account/deposit':
-      return { ...state, balance: state.balance + action.payload };
+      return {
+        ...state,
+        balance: state.balance + action.payload,
+        isLoading: false,
+      };
     case 'account/withdraw':
       return { ...state, balance: state.balance - action.payload };
     case 'account/requestLoan':
@@ -31,8 +38,23 @@ export default function accountReducer(state = initialStateAccount, action) {
   }
 }
 
-export function deposit(amount) {
-  return { type: 'account/deposit', payload: amount };
+export function deposit(amount, currency) {
+  if (currency === 'USD') return { type: 'account/deposit', payload: amount };
+
+  return async function (dispatch, getState) {
+    dispatch({ type: 'account/convertingCurrency' });
+
+    // api call
+    const host = 'api.frankfurter.app';
+    const res = await fetch(
+      `https://${host}/latest?amount=${amount}&from=${currency}&to=USD`
+    );
+    const data = await res.json();
+    const converted = data.rates.USD;
+
+    // return
+    dispatch({ type: 'account/deposit', payload: converted });
+  };
 }
 export function withdraw(amount) {
   return { type: 'account/withdraw', payload: amount };
