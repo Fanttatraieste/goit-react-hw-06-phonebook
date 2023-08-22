8cf878028bf5deda3ca6f87adc3cac52f81c5ea9
Fanttatraieste <118627923+Fanttatraieste@users.noreply.github.com>
2023-08-22 18:16:31 +0300

    bug fix

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import accountReducer from './features/account/accountSlice';
 import customerReducer from './features/customer/customerSlice';
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 import { configureStore } from '@reduxjs/toolkit';
 
 const store = configureStore({
